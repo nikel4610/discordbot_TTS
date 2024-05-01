66414b6b4ef8c27ca0bf1e87e507c67dafc1c0dd
Hyun-woo <nikel4610@gmail.com>
2024-05-01 15:16:59 +0900

    tts bot alpha ver

--- TTS_bot.py
+++ TTS_bot.py
@@ -1,24 +1,58 @@
 import asyncio
 import os
-import time
 
-from discord.ext.commands import Bot
+import discord
+from discord.ext import commands
 from dotenv import load_dotenv
 from navertts import NaverTTS
-import discord
 
 load_dotenv()
-# https://discord.com/oauth2/authorize?client_id=1234120588877107240&permissions=2168832&scope=bot
 
 BOT_TOKEN = os.getenv('BOT_TOKEN')
-VOICE_CHANNEL_ID = os.getenv('VOICE_CHANNEL_ID')
+TARGET_CHANNEL_ID = int(os.getenv('TARGET_CHANNEL_ID'))
 
 intents = discord.Intents.default()
 intents.message_content = True
 
-bot = Bot('', intents=intents)
+bot = commands.Bot(command_prefix='', intents=intents)
 
-disconnect_timer = None
+
+async def speak_message(message, voice_client):
+    tts = NaverTTS(message.content, speed=0)
+    tts.save('tts.mp3')
+
+    if voice_client.is_playing():
+        voice_client.stop()
+
+    voice_client.play(discord.FFmpegPCMAudio('tts.mp3'), after=lambda e: print('done', e))
+
+    while voice_client.is_playing():
+        await asyncio.sleep(1)
+
+
+async def voice_disconnect_after_delay(voice_client, delay):
+    await asyncio.sleep(delay)
+    await voice_disconnect(voice_client)
+
+
+async def voice_disconnect(voice_client, message_channel):
+    if voice_client.is_connected():
+        tts = NaverTTS('저는 이만 들어가볼게요', speed=0)
+        tts.save('tts.mp3')
+
+        voice_client.play(discord.FFmpegPCMAudio('tts.mp3'))
+
+        await asyncio.sleep(3)
+
+        await voice_client.disconnect()
+        await message_channel.send('음성 채널에서 나갔어요!')
+
+
+async def find_voice_channel(guild, user):
+    for vc in guild.voice_channels:
+        if user in [member for member in vc.members if not member.bot]:
+            return vc
+    return None
 
 
 @bot.event
@@ -28,52 +62,30 @@ async def on_ready():
 
 @bot.event
 async def on_message(message):
-    global disconnect_timer
-
     if message.author.bot:
         return
 
-    if bot.voice_clients == []:
-        channel = bot.get_channel(int(VOICE_CHANNEL_ID))
-        await channel.connect()
+    if message.channel.id == TARGET_CHANNEL_ID:
+        if message.content == '!leave':
+            voice_client = discord.utils.get(bot.voice_clients, guild=message.guild)
+            if voice_client:
+                await voice_disconnect(voice_client, message.channel)
+            else:
+                await message.channel.send('음성 채널에 접속 중이 아니에요!')
+        else:
+            if not bot.voice_clients:
+                voice_channel = await find_voice_channel(message.guild, message.author)
+                if voice_channel:
+                    await voice_channel.connect()
+                else:
+                    await message.channel.send('음성 채널을 찾을 수 없어요!')
+                    return
 
-    voice = bot.voice_clients[0]
+            voice_client = bot.voice_clients[0]
 
-    tts = NaverTTS(message.content, speed=0)
-    tts.save('tts.mp3')
+            await speak_message(message, voice_client)
 
-    if voice.is_playing():
-        voice.stop()
-
-    voice.play(discord.FFmpegPCMAudio('tts.mp3'), after=lambda e: print('done', e))
-
-    while voice.is_playing():
-        await asyncio.sleep(1)
-
-    if disconnect_timer:
-        disconnect_timer.cancel()
-
-    disconnect_timer = asyncio.create_task(voice_disconnect_after_delay(5.0))
-
-
-async def voice_disconnect_after_delay(delay):
-    await asyncio.sleep(delay)
-    await voice_disconnect()
+            await voice_disconnect_after_delay(voice_client, 180.0)
 
 
-async def voice_disconnect():
-    global disconnect_timer
-
-    voice = bot.voice_clients[0]
-    if voice.is_connected():
-        tts = NaverTTS('저는 이만 들어가볼게요', speed=0)
-        tts.save('tts.mp3')
-
-        voice.play(discord.FFmpegPCMAudio('tts.mp3'))
-
-        time.sleep(3)
-
-        await voice.disconnect()
-
-asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 bot.run(BOT_TOKEN)
